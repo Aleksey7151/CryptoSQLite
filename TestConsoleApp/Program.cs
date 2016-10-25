3e4bc5c28b77f7c5c5099649e72019f84fb51dd3
Alexei <safonov7151@gmail.com>
2016-10-25 12:41:34 +0300

    Insert function tested on Windows 8.1

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CryptoSQLite;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        [CryptoTable("Jobs")]
+        public class Jobs
+        {
+            [Column("id"), PrimaryKey]
+            public int Id { get; set; }
+
+            [Encrypted]
+            public string JobName { get; set; }
+
+            [Encrypted]
+            public string Description { get; set; }
+
+            public bool IsCompleted { get; set; }
+        }
+
+        [CryptoTable("Work")]
+        public class Work
+        {
+            public int ID { get; set; }
+        }
+
+        [CryptoTable("Employee")]
+        public class Employee
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            var key = new byte[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32};
+            using (var db = new CryptoSQLite.CryptoSQLite("myDb.db3"))
+            {
+                //db.CreateTable<Jobs>();
+                db.SetEncryptionKey(key);
+                var item = new Jobs
+                {
+                    Id = 66,
+                    JobName = "Finish CryptoSQLite.",
+                    Description = "It is wery difficult Job.",
+                    IsCompleted = false
+                };
+
+                db.Insert(item);
+            }
+           
+        }
+    }
+}
