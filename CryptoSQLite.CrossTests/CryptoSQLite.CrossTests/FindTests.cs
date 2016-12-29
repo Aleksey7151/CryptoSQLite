8973b3ae4902abf46d057abec148cd9c349a3dc3
Alexei <safonov7151@gmail.com>
2016-12-29 14:18:51 +0300

    CrossTests updated according to new ICryptoSQLite interface defenition.

--- CryptoSQLite.CrossTests/CryptoSQLite.CrossTests/FindTests.cs
+++ CryptoSQLite.CrossTests/CryptoSQLite.CrossTests/FindTests.cs
@@ -46,119 +46,7 @@ namespace CryptoSQLite.CrossTests
                 }
             }
         }
-
-        [Test]
-        public async Task FindFunctionUsingUpperValue()
-        {
-            var accounts = GetAccounts();
-            foreach (var db in GetAsyncConnections())
-            {
-                try
-                {
-                    await db.DeleteTableAsync<AccountsData>();
-                    await db.CreateTableAsync<AccountsData>();
-
-                    foreach (var account in accounts)
-                        await db.InsertItemAsync(account);
-
-                    var result = await db.FindAsync<AccountsData>("Age", null, 23);
-
-                    var table = result.ToArray();
-                    Assert.IsTrue(table.Length == 4);
-                    Assert.IsTrue(table[0].Equal(accounts[0]));
-                    Assert.IsTrue(table[1].Equal(accounts[1]));
-                    Assert.IsTrue(table[2].Equal(accounts[2]));
-                    Assert.IsTrue(table[3].Equal(accounts[5]));
-
-                }
-                catch (CryptoSQLiteException cex)
-                {
-                    Assert.Fail(cex.Message + cex.ProbableCause);
-                }
-                catch (Exception ex)
-                {
-                    Assert.Fail(ex.Message);
-                }
-                finally
-                {
-                    db.Dispose();
-                }
-            }
-        }
-
-        [Test]
-        public async Task FindFunctionUsingLowerValue()
-        {
-            var accounts = GetAccounts();
-            foreach (var db in GetAsyncConnections())
-            {
-                try
-                {
-                    await db.DeleteTableAsync<AccountsData>();
-                    await db.CreateTableAsync<AccountsData>();
-
-                    foreach (var account in accounts)
-                        await db.InsertItemAsync(account);
-
-                    var result = await db.FindAsync<AccountsData>("Age", 27);
-
-                    var table = result.ToArray();
-                    Assert.IsTrue(table.Length == 3);
-                    Assert.IsTrue(table[0].Equal(accounts[3]));
-                    Assert.IsTrue(table[1].Equal(accounts[4]));
-                    Assert.IsTrue(table[2].Equal(accounts[7]));
-                }
-                catch (CryptoSQLiteException cex)
-                {
-                    Assert.Fail(cex.Message + cex.ProbableCause);
-                }
-                catch (Exception ex)
-                {
-                    Assert.Fail(ex.Message);
-                }
-                finally
-                {
-                    db.Dispose();
-                }
-            }
-        }
-
-        [Test]
-        public async Task FindFunctionUsingLowerAndUpperValues()
-        {
-            var accounts = GetAccounts();
-            foreach (var db in GetAsyncConnections())
-            {
-                try
-                {
-                    await db.DeleteTableAsync<AccountsData>();
-                    await db.CreateTableAsync<AccountsData>();
-
-                    foreach (var account in accounts)
-                        await db.InsertItemAsync(account);
-
-                    var result = await db.FindAsync<AccountsData>("Age", 20, 24);
-
-                    var table = result.ToArray();
-                    Assert.IsTrue(table.Length == 3);
-                    Assert.IsTrue(table[0].Equal(accounts[0]));
-                    Assert.IsTrue(table[1].Equal(accounts[1]));
-                    Assert.IsTrue(table[2].Equal(accounts[5]));
-                }
-                catch (CryptoSQLiteException cex)
-                {
-                    Assert.Fail(cex.Message + cex.ProbableCause);
-                }
-                catch (Exception ex)
-                {
-                    Assert.Fail(ex.Message);
-                }
-                finally
-                {
-                    db.Dispose();
-                }
-            }
-        }
+        
 
         [Test]
         public async Task FindByValueFunction()
@@ -195,74 +83,7 @@ namespace CryptoSQLite.CrossTests
             }
         }
 
-        [Test]
-        public async Task FindFunctionWithoutUsingValues()
-        {
-            var accounts = GetAccounts();
-            foreach (var db in GetAsyncConnections())
-            {
-                try
-                {
-                    await db.DeleteTableAsync<AccountsData>();
-                    await db.CreateTableAsync<AccountsData>();
-
-                    foreach (var account in accounts)
-                        await db.InsertItemAsync(account);
-
-                    var result = await db.FindAsync<AccountsData>("Age");
-
-                    var table = result.ToArray();
-
-                    Assert.IsTrue(table.Length == accounts.Length);
-
-                    for (var i = 0; i < accounts.Length; i++)
-                        Assert.IsTrue(table[i].Equal(accounts[i]));
-
-                }
-                catch (CryptoSQLiteException cex)
-                {
-                    Assert.Fail(cex.Message + cex.ProbableCause);
-                }
-                catch (Exception ex)
-                {
-                    Assert.Fail(ex.Message);
-                }
-                finally
-                {
-                    db.Dispose();
-                }
-            }
-        }
-
-
-        [Test]
-        public async Task FindFunctionUsingInvalidColumnName()
-        {
-            foreach (var db in GetAsyncConnections())
-            {
-                try
-                {
-                    await db.DeleteTableAsync<AccountsData>();
-                    await db.CreateTableAsync<AccountsData>();
-
-                    await db.FindAsync<AccountsData>("Agee");
-                }
-                catch (CryptoSQLiteException cex)
-                {
-                    Assert.IsTrue(cex.Message.IndexOf("doesn't contain column", StringComparison.Ordinal) > 0);
-                    return;
-                }
-                catch (Exception ex)
-                {
-                    Assert.Fail(ex.Message);
-                }
-                finally
-                {
-                    db.Dispose();
-                }
-                Assert.Fail();
-            }
-        }
+        
 
         [Test]
         public async Task FindByValueFunctionUsingInvalidColumnName()
@@ -279,35 +100,6 @@ namespace CryptoSQLite.CrossTests
                 catch (CryptoSQLiteException cex)
                 {
                     Assert.IsTrue(cex.Message.IndexOf("doesn't contain column", StringComparison.Ordinal) > 0);
-                    return;
-                }
-                catch (Exception ex)
-                {
-                    Assert.Fail(ex.Message);
-                }
-                finally
-                {
-                    db.Dispose();
-                }
-                Assert.Fail();
-            }
-        }
-
-        [Test]
-        public async Task FindFunctionUsingNullColumnName()
-        {
-            using (var db = GetGostAsyncConnection())
-            {
-                try
-                {
-                    await db.DeleteTableAsync<AccountsData>();
-                    await db.CreateTableAsync<AccountsData>();
-
-                    await db.FindAsync<AccountsData>(null, null, null);
-                }
-                catch (CryptoSQLiteException cex)
-                {
-                    Assert.IsTrue(cex.Message.IndexOf("Column name can't be null.", StringComparison.Ordinal) >= 0);
                     return;
                 }
                 catch (Exception ex)
@@ -351,34 +143,7 @@ namespace CryptoSQLite.CrossTests
             }
         }
 
-        [Test]
-        public async Task FindFunctionUsingEncryptedColumnIsForbidden()
-        {
-            foreach (var db in GetAsyncConnections())
-            {
-                try
-                {
-                    await db.DeleteTableAsync<AccountsData>();
-                    await db.CreateTableAsync<AccountsData>();
-                    var result = await db.FindAsync<AccountsData>("Password");
-                }
-                catch (CryptoSQLiteException cex)
-                {
-                    Assert.IsTrue(cex.Message.IndexOf("has [Encrypted] attribute, so this column is encrypted. Find function can't work with encrypted columns", StringComparison.Ordinal) > 0);
-                    return;
-                }
-                catch (Exception ex)
-                {
-                    Assert.Fail(ex.Message);
-                }
-                finally
-                {
-                    db.Dispose();
-                }
-                Assert.Fail();
-            }
-        }
-
+       
         [Test]
         public async Task FindByValueFunctionUsingEncryptedColumnIsForbidden()
         {
